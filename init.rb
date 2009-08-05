b9ee0532ceba7c9b2c7c03b5e15d23adfe8171e8
Nat Budin <natbudin@gmail.com>
2009-08-05 10:57:26 -0400

    Show signup link when logged out

--- init.rb
+++ init.rb
@@ -7,4 +7,5 @@ require 'paywall_helpers'
 config.gem 'money'
 config.gem 'google4r-checkout', :lib => 'google4r/checkout'
 Journey::QuestionnaireExtensions.register_extension(JourneyPaywall::QuestionnaireExtensions)
+Journey::UserOptions.add_logged_out_option("Subscribe", {:controller => "subscriptions", :action => "index"})
 Journey::UserOptions.add_logged_in_option("Subscription", {:controller => "subscriptions", :action => "index"})
