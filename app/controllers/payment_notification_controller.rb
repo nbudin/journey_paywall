e812c74ff55e3e9610fdaff6b81be7df6efb2e51
Nat Budin <natbudin@gmail.com>
2009-08-27 17:49:05 -0400

    Accept and process Google notifications (level 2 integration)

--- app/controllers/payment_notification_controller.rb
+++ app/controllers/payment_notification_controller.rb
@@ -1,5 +1,41 @@
 class PaymentNotificationController < ApplicationController
   def google
-    head :ok
+    authenticate_or_request_with_http_basic("Google Checkout notification endpoint") do |merchant_id, merchant_key|
+      unless PaymentMethods::GoogleSubscription.merchant_credentials_match?(merchant_id, merchant_key)
+        return head :unauthorized
+      end
+    end
+    
+    frontend = PaymentMethods::GoogleSubscription.frontend
+    handler = frontend.create_notification_handler
+        
+    begin
+       notification = handler.handle(request.raw_post) # raw_post contains the XML
+    rescue Google4R::Checkout::UnknownNotificationType
+       # This can happen if Google adds new commands and Google4R has not been
+       # upgraded yet. It is not fatal.
+       render :text => 'ignoring unknown notification type', :status => 200
+       return
+    end
+    
+    if notification.kind_of? NewOrderNotification
+      notification.shopping_cart.items.each do |item|
+        if item.private_data and item.private_data[:subscription_id]
+          @subscription = Subscription.find(item.private_data[:subscription_id])
+
+          @gs = @subscription.payment_method
+          @gs.google_order_number = notification.google_order_number
+          @gs.financial_order_state = notification.financial_order_state
+          @gs.save
+        end
+      end
+    elsif notification.kind_of? OrderStateChangeNotification
+      @gs = PaymentMethods::GoogleSubscription.find_by_google_order_number(notification.google_order_number)
+      @gs.financial_order_state = notification.new_financial_order_state
+      @gs.save
+    end
+    
+    notification_acknowledgement = Google4R::Checkout::NotificationAcknowledgement.new(notification)
+    render :xml => notification_acknowledgement, :status => 200
   end
 end
