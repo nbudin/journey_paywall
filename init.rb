50a1c02af835dd87d307f49de1edd7718cdd3a93
Nat Budin <natbudin@gmail.com>
2009-08-31 11:32:26 -0400

    Require at least version 1.0.8 of nbudin-google4r-checkout

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ require 'paywall_helpers'
 require 'yaml'
 
 config.gem 'money'
-config.gem 'nbudin-google4r-checkout', :lib => 'google4r/checkout', :source => 'http://gems.github.com'
+config.gem 'nbudin-google4r-checkout', :lib => 'google4r/checkout', :source => 'http://gems.github.com', :version => "~> 1.0.8"
 Journey::QuestionnaireExtensions.register_extension(JourneyPaywall::QuestionnaireExtensions)
 Journey::UserOptions.add_logged_out_option("Subscribe", {:controller => "subscriptions", :action => "index"})
 Journey::UserOptions.add_logged_in_option("Subscription", {:controller => "subscriptions", :action => "index"})
