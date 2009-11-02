6f292540ae9f6866bc146ab7d797ba5078578ab0
Nat Budin <natbudin@gmail.com>
2009-11-02 15:55:56 -0500

    Dashbox widget

--- init.rb
+++ init.rb
@@ -12,6 +12,8 @@ Journey::QuestionnaireExtensions.register_extension(JourneyPaywall::Questionnair
 Journey::UserOptions.add_logged_out_option("Subscribe", {:controller => "subscriptions", :action => "index"})
 Journey::UserOptions.add_logged_in_option("Subscription", {:controller => "subscriptions", :action => "index"})
 
+Journey::Dashboard.add_dashbox("subscriptions/dashbox", :right)
+
 Journey::SiteOptions.default_layout = "journey_with_paywall"
 
 yamlpath = "#{RAILS_ROOT}/config/journey_paywall.yml"
