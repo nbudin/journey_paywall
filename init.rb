d402b078c54d2c732dc019a907570a1a4fc594d3
Nat Budin <natbudin@gmail.com>
2010-06-25 13:28:15 -0400

    We're using Bundler now, no need for config.gem decls

--- init.rb
+++ init.rb
@@ -4,10 +4,8 @@ require 'journey_paywall'
 require 'journey_questionnaire'
 require 'paywall_helpers'
 require 'yaml'
+require 'google4r/checkout'
 
-config.gem 'money'
-config.gem 'nbudin-google4r-checkout', :lib => 'google4r/checkout', :source => 'http://gemcutter.org',
-           :version => "~> 1.0.11"
 Journey::QuestionnaireExtensions.register_extension(JourneyPaywall::QuestionnaireExtensions)
 Journey::UserOptions.add_logged_out_option("Subscribe", {:controller => "subscriptions", :action => "index"})
 Journey::UserOptions.add_logged_in_option("Subscription", {:controller => "subscriptions", :action => "index"})
